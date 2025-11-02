9059b2a2dd0e3d7c118cb9e57c84196b9e32f4dd
sayoun555 <zzangsang523@gmail.com>
2025-11-02 19:52:00 +0900

    feat: main 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.config.AppConfig;
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-
+        AppConfig appConfig = new AppConfig();
+        LottoController lottoController = appConfig.lottoController();
+        lottoController.run();
     }
 }
